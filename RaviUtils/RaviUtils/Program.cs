2418d8d0f28c1f9594382212bb24e67d4f798494
Ravindar <ryenugu@gmail.com>
2016-06-16 19:30:49 -0400

    isPrime done

--- RaviUtils/RaviUtils/Program.cs
+++ RaviUtils/RaviUtils/Program.cs
@@ -1,17 +1,45 @@
 ﻿using System;
+using RaviUtils;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = 337;
+        Console.WriteLine(Basics.isPrime(n));
+    }
+}
+
+
 namespace RaviUtils
 {
-    class Program
+    static class Basics
     {
-        static void Main(string[] args)
+        /* Usage: Checks if a number n is prime
+           Description: Check if n is divisible by any non even number upto Sqrt of n */
+
+        public static bool isPrime(int n)
         {
-            int n = 13;
-            Console.WriteLine(n);
+            bool flag = false;
+            if (n == 2)
+                return true;
+            for (int i = 3; i < Math.Sqrt(n); i += 2)
+            {
+                if (n % i == 0)
+                {
+                    flag = false;
+                    break;
+                }
+                flag = true;
+            }
+
+            return flag;
         }
     }
+
 }
